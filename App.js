09ec537d2a6250ec14b7ac65c82a83cbd699e0f9
allexandrahu <hu.ale@northeastern.edu>
2024-04-02 07:16:17 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import express from "express";
+import Lab5 from "./Lab5.js";
+import CourseRoutes from "./Kanbas/courses/routes.js";
+import ModuleRoutes from "./Kanbas/modules/routes.js"; 
+import cors from "cors";
+const app = express();
+app.use(cors());
+CourseRoutes(app);
+app.use(express.json());
+ModuleRoutes(app);
+Lab5(app);
+app.listen(process.env.PORT || 4000);
---
+++ Kanbas/Database/courses.js
@@ -0,0 +1,57 @@
+export default [  {
+    "_id": "RS101",
+    "name": "Distributed Systems",
+    "number": "CS3700",
+    "startDate": "2023-01-10",
+    "endDate": "2023-05-15",
+    "image": "networks.jpeg"
+  },
+  {
+    "_id": "RS102",
+    "name": "Cybersecurity",
+    "number": "CY2550",
+    "startDate": "2023-01-10",
+    "endDate": "2023-05-15",
+    "image": "cybersecurity.jpg"
+  },
+  {
+    "_id": "RS103",
+    "name": "Accounting",
+    "number": "BUSN1200",
+    "startDate": "2023-01-10",
+    "endDate": "2023-05-15",
+    "image": "account.jpg"
+  },
+  {
+    "_id": "RS104",
+    "name": "User Experience Design",
+    "number": "CS4973",
+    "startDate": "2023-01-10",
+    "endDate": "2023-05-15",
+    "image": "img.jpeg"
+  },
+  {
+    "_id": "RS105",
+    "name": "Calculus",
+    "number": "MATH1341",
+    "startDate": "2023-01-10",
+    "endDate": "2023-05-15",
+    "image": "calc.jpg"
+  },
+  {
+    "_id": "RS106",
+    "name": "Co-op Course",
+    "number": "CS1210",
+    "startDate": "2023-01-10",
+    "endDate": "2023-05-15",
+    "image": "isec.jpeg"
+  },
+  {
+    "_id": "RS107",
+    "name": "React",
+    "number": "CS3500",
+    "startDate": "2023-01-10",
+    "endDate": "2023-05-15",
+    "image": "react.webp"
+  }
+]
